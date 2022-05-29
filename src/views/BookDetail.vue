aae7382fd531ec7e0fe6b5612026c6c49bdfd88a
Valdis Wong <457644009@qq.com>
2022-05-29 22:47:49 +0800

    更新了检索功能，暂时通过proxy访问其他本地端口进行数据库查询。

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="book-detail">
-    <!-- 古籍详情页 -->
+    <p v-text="$route.params.bookID"></p>
+    <h1 v-text="book_data.content"></h1>
+    <p v-text="book_data.detail"></p>
+    <img src="@/assets/placeholder.jpg" alt="" />
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "BookDetail",
+  data() {
+    return {
+      book_data: {},
+    };
+  },
+  mounted() {
+    axios.get(`/data/book-detail/${this.$route.params.bookID}`).then((d) => {
+      this.book_data = d.data[0];
+    });
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.book-detail {
+  background-color: #f2e0c4;
+  width: 100vw;
+  height: 100vh;
+  img {
+    width: 300px;
+  }
+}
+</style>
