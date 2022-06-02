d38760d5cb631f5e8497437554cdc82c9f621584
Valdis Wong <457644009@qq.com>
2022-06-02 09:12:20 +0800

    鼠标悬浮可显示古籍信息

---
+++ src/components/BookInfoDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="book-info-dialog">
+    <h4>
+      <span v-text="'#' + id"></span>
+      <span v-text="title"></span>
+    </h4>
+    <p v-text="detail"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookInfoDialog",
+  props: {
+    id: String,
+    title: String,
+    detail: String,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.book-info-dialog {
+  background-color: #333;
+  padding: 1.5rem;
+  max-width: 15rem;
+  position: absolute;
+  color: #fff;
+  display: none;
+  h4 {
+    font-size: 0.9rem;
+    span {
+      display: block;
+    }
+  }
+  p {
+    font-size: 0.7rem;
+    color: #ccc;
+  }
+}
+</style>
