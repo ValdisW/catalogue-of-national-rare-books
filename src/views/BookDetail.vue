fa1b1e1ede022a64d187b8ab639d9f7360deb59b
Kong Fanyu <freja.kong@outlook.com>
2022-08-09 16:57:05 +0800

    古籍詳情切換

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -2,13 +2,32 @@
   <div class="book-detail">
     <BackButton />
     <div class="content">
-
       <div class="info">
         <div class="title">
           <!--          <span v-text="$route.params.bookID"></span>-->
           <span v-text="normalized_title"></span>
         </div>
-        <div class="detail">
+        <div class="switch">
+          <button
+            class="book-content"
+            ref="switch-detail"
+            v-on:click="switch_content()"
+          >
+            名錄内容
+          </button>
+          <div>│</div>
+          <button
+            class="book-display"
+            ref="switch-content"
+            v-on:click="switch_display()"
+          >
+            字段顯示
+          </button>
+        </div>
+        <div class="display" v-show="isDisplay">
+          <p class="document-type" v-text="book_data.content"></p>
+        </div>
+        <div class="detail" v-show="isDetail">
           <div class="detail-title">
             <p>文種：</p>
             <p>文獻類型：</p>
@@ -23,51 +42,64 @@
           <div class="detail-content">
             <p class="language" v-text="book_data.language"></p>
             <p class="document-type" v-text="book_data.document_type"></p>
-            <p class="edition-type" >-</p>
-            <p class="frame-size" >-</p>
-            <p class="binding-form" >-</p>
-            <p class="book-size" >-</p>
-            <p class="note" >-</p>
-            <p class="institute" style="margin-top: 20px" v-text="book_data.institution"></p>
-            <p v-text="book_data.call_number" ></p>
-
+            <p class="edition-type">-</p>
+            <p class="frame-size">-</p>
+            <p class="binding-form">-</p>
+            <p class="book-size">-</p>
+            <p class="note">-</p>
+            <p
+              class="institute"
+              style="margin-top: 20px"
+              v-text="book_data.institution"
+            ></p>
+            <p v-text="book_data.call_number"></p>
           </div>
         </div>
-        <div class="related-books">
-          <p
-            v-for="b in this.related_books"
-            :key="b.名录ID"
-            v-text="
-              $store.state.allData
-                .find((elem) => elem.id == b.名录ID)
-                .content.split('　')[0]
-            "
-          ></p>
-        </div>
-        <div class="person">
-          <table>
-            <tr v-for="e in person_data" :key="e">
-              <td>
-                <router-link
-                  v-if="e.责任人姓名"
-                  v-text="
-                    '[' + (e.dynasty_or_nation || '？') + ']' + e.责任人姓名
-                  "
-                  :to="'/person-detail/' + e.责任人姓名"
-                ></router-link>
-                <span
-                  v-else
-                  v-text="'[' + (e.dynasty_or_nation || '？') + ']佚名'"
-                ></span>
-              </td>
-              <td v-text="e.责任行为"></td>
-            </tr>
-          </table>
-        </div>
+        <!--        <div class="related-books">-->
+        <!--          <p-->
+        <!--            v-for="b in this.related_books"-->
+        <!--            :key="b.名录ID"-->
+        <!--            v-text="-->
+        <!--              $store.state.allData-->
+        <!--                .find((elem) => elem.id == b.名录ID)-->
+        <!--                .content.split('　')[0]-->
+        <!--            "-->
+        <!--          ></p>-->
+        <!--        </div>-->
+        <!--        <div class="person">-->
+        <!--          <table>-->
+        <!--            <tr v-for="e in person_data" :key="e">-->
+        <!--              <td>-->
+        <!--                <router-link-->
+        <!--                  v-if="e.责任人姓名"-->
+        <!--                  v-text="-->
+        <!--                    '[' + (e.dynasty_or_nation || '？') + ']' + e.责任人姓名-->
+        <!--                  "-->
+        <!--                  :to="'/person-detail/' + e.责任人姓名"-->
+        <!--                ></router-link>-->
+        <!--                <span-->
+        <!--                  v-else-->
+        <!--                  v-text="'[' + (e.dynasty_or_nation || '？') + ']佚名'"-->
+        <!--                ></span>-->
+        <!--              </td>-->
+        <!--              <td v-text="e.责任行为"></td>-->
+        <!--            </tr>-->
+        <!--          </table>-->
+        <!--        </div>-->
       </div>
-      <div class="img-wrapper">
-        <img src="@/assets/placeholder.jpg" alt="" />
+
+      <div class="book-image">
+        <div class="switch_pic" style="text-align: center">
+         <button class="firstBtn" ref="firstBtn" v-on:click="switch_first()"></button>
+         <button class="secondBtn" ref="secondBtn"  v-on:click="switch_second()"></button>
+        </div>
+      <div class="img-wrapper" >
+        <img class="img-1" v-show="firstPic" src="@/assets/placeholder.jpg" alt="" />
+           <!--  test the width-->
+        <img class="img-2" v-show="secPic" src="https://file.szmuseum.com/ThumbCover/%E5%8F%A4%E7%B1%8D%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86/201806141352095MD1WD.jpg" alt="" />
       </div>
+      </div>
+
     </div>
     <BookInfoDialog
       ref="book-info-dialog"
@@ -89,6 +121,10 @@ export default {
   components: { BookInfoDialog, BackButton },
   data() {
     return {
+      isDetail: true,
+      isDisplay: false,
+      firstPic: true,
+      secPic: false,
       book_data: { content: "" },
       hover_data: {
         id: "",
@@ -99,6 +135,37 @@ export default {
       seal_data: [],
       related_books: [],
     };
+  },
+  methods: {
+    switch_content() {
+      this.isDisplay = false;
+      this.isContent = true;
+
+      this.$refs["switch-content"].style.color = "#4a3300";
+      this.$refs["switch-detail"].style.color = "#8f644d";
+    },
+
+    switch_display() {
+      this.isDisplay = true;
+      this.isContent = false;
+
+      this.$refs["switch-content"].style.color = "#8f644d";
+      this.$refs["switch-detail"].style.color = "#4a3300";
+    },
+
+    switch_first(){
+      this.firstPic = true;
+      this.secPic = false;
+      this.$refs["firstBtn"].style.background = "#42230f";
+      this.$refs["secondBtn"].style.background = "transparent";
+    },
+
+    switch_second(){
+      this.secPic = true;
+      this.firstPic = false;
+      this.$refs["firstBtn"].style.background = "transparent";
+      this.$refs["secondBtn"].style.background = "#42230f";
+    },
   },
   computed: {
     normalized_title() {
@@ -158,7 +225,6 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
 .book-detail {
   width: 100vw;
   height: 100vh;
@@ -172,19 +238,85 @@ export default {
     align-items: center;
     justify-content: center;
 
-    .img-wrapper {
-      background: #dec4a4;
-      // background: #5e524a;
+    .display {
+      height: 80vh;
+      font-size: 2.3vh;
+      margin-top: 5vh;
+    }
+    .book-image{
+      width: 45%;
       height: 75vh;
-      display: flex;
       align-items: center;
-      margin: 0 0 0 2rem;
-      img {
-        width: 500px;
+      justify-content: center;
+      .switch_pic{
+        display: block;
+        margin: 0 auto;
+        .firstBtn {
+          margin: 8px;
+          width: 15px;
+          height: 15px;
+          background: #42230f;
+          border-radius: 50%;
+          border: 1px solid #42230f;
+
+        }
+        .secondBtn {
+          margin: 8px;
+          width: 15px;
+          height: 15px;
+          background: transparent;
+          border-radius: 50%;
+          border: 1px solid #42230f;
+        }
+      }
+
+      .img-wrapper {
+        background: #dec4a4;
+        // background: #5e524a;
+        height: 60vh;
+        margin: 1rem 0 0 0;
+        img {
+          height: 60vh;
+          transition: all 1s ease-in-out;
+        }
+
+        .img-1 {
+          margin: 0 auto;
+          display: block;
+        }
+
+
+        .img-2 {
+          margin: 0 auto;
+          display: block;
+
+        }
       }
     }
+
+
     .info {
+      margin-right: 5vh;
+      width: 40%;
       height: 80vh;
+      .switch {
+        display: flex;
+
+        margin-bottom: 10px;
+
+        .book-content {
+          color: #8f644d;
+          background-color: transparent;
+          border-style: none;
+          font-size: 2.3vh;
+        }
+        .book-display {
+          color: #4a3300;
+          background-color: transparent;
+          border-style: none;
+          font-size: 2.3vh;
+        }
+      }
       .title {
         font-family: "SourceHanSerif";
         font-weight: bold;
@@ -202,8 +334,9 @@ export default {
         font-size: 0.8rem;
         display: flex;
         line-height: 2.5;
-        .detail-title{
-          width:30%;
+        .detail-title {
+          text-align: right;
+          width: 20%;
           font-weight: bold;
         }
       }
