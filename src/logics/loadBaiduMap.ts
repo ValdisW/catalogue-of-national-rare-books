91c88f413b039300c9d732fb54148368aa17a2df
ValdisW <457644009@qq.com>
2024-03-30 14:16:34 +0800

    remove unused file

--- src/logics/loadBaiduMap.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from "vue";
-import BaiduMap from "vue-baidu-map-3x";
-
-export function loadBaiduMap(app: App) {
-  app.use(BaiduMap, {
-    ak: "xsfg4NDlGqYMfYty9IRTLGCoxtMrrp8g",
-  });
-}
